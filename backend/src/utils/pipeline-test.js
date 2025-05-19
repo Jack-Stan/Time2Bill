226186675e44e9ce4cb3e51b99ad608b423cd061
Jack-stan <stanverbruggen@protonmail.com>
2025-05-19 14:16:52 +0200

    Test CI/CD pipeline with Firebase Service Account

--- backend/src/utils/pipeline-test.js
+++ backend/src/utils/pipeline-test.js
@@ -1,5 +1,6 @@
 // Dit is een testbestand om de CI/CD pipeline te testen
 // Bijgewerkt op: 19 mei 2025
+// Pipeline met Firebase Service Account is toegevoegd
 export function testFunction() {
-  return "Pipeline Test - Updated";
+  return "Pipeline Test - With Firebase Service Account";
 }
